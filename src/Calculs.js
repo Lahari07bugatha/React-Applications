73328e446fc72266b59a27d89733baa3a9bf9bf3
LAHARI BUGATHA <laharibugatha07@gmail.com>
2025-12-14 19:05:43 +0530

    calculator application

---
+++ src/Calculs.js
@@ -0,0 +1,33 @@
+import Keypads from "./Keypads"
+import React,{useState} from "react"
+function Appp(){
+
+    let [input,setInput]=useState('')
+
+   function handleClick(value){
+       setInput(input+value)
+   }
+   function calculate(value){
+     let outputval= eval(input)
+     setInput(outputval)
+   }
+    
+   function handleClear(){
+         setInput('')
+   }
+
+
+
+    return(
+        <>
+         <div className="container"> 
+            <h2>Calculator using react</h2>
+            <div className="calclus">
+                <input type="text" className="output" value={input}  />
+                <Keypads handleClick={handleClick} calculate={calculate} handleClear={handleClear}/>
+            </div>
+         </div>
+        </>
+    )
+}
+export default Appp
